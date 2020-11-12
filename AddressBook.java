3373815e17fe5c493f076e81cd69701dd8846984
amtksms97 <amtksms97@gmail.com>
2020-11-12 11:21:06 +0530

    Added: Code to Enter the details of the user

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,24 @@
+import java.util.*;
 public class AddressBook {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Address Book Program");
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter user's first name");
+		String first= sc.nextLine(); 
+		System.out.println("Enter user's second name");
+		String second= sc.nextLine(); 
+		System.out.println("Enter user's address");
+		String address= sc.nextLine();
+		System.out.println("Enter user's city name");
+		String city= sc.nextLine();
+		System.out.println("Enter user's state name");
+		String state= sc.nextLine();
+		System.out.println("Enter user's state zip code");
+		int zip= sc.nextInt();
+		System.out.println("Enter user's phone number");
+		long phone= sc.nextLong();
+		System.out.println(first +second);
 	}
 
 }
+
